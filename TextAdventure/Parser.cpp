07fe1a0695c339129c2afc732448e1a895792c97
Justin LeCheminant <jlechem@gmail.com>
2017-12-28 17:17:00 -0700

    Adding parser class.

---
+++ TextAdventure/Parser.cpp
@@ -0,0 +1,21 @@
+/*
+	Parser.cpp
+	Created By:		Justin LeCheminant
+	Created On:		12-28-2017
+	Last Modified:	12-28-2017
+
+	Notes: Implementation of the parser class.
+
+*/
+
+#include "stdafx.h"
+
+#include "Parser.h"
+
+Parser::Parser()
+{
+}
+
+Parser::~Parser()
+{
+}
---
+++ TextAdventure/Parser.h
@@ -0,0 +1,26 @@
+/*
+	Parser.h
+	Created By:		Justin LeCheminant
+	Created On:		12-28-2017
+	Last Modified:	12-28-2017
+
+	Notes: This is the parser class used to parse a players input into something manageable we can use to create
+	commands, actions, etc.
+
+*/
+
+#pragma once
+
+class Parser
+{
+public:
+	Parser();
+	~Parser();
+
+private:
+
+	string _verb;
+	string _noun;
+	string _adjective;
+
+};
